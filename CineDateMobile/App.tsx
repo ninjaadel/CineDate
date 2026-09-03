8dacb503394954dec1c36d5c757e16893827c687
ninjaadel <iurchenkoadel@gmail.com>
2026-09-03 16:30:30 +0300

    roomscreeni tamamladım

--- CineDateMobile/App.tsx
+++ CineDateMobile/App.tsx
@@ -1,45 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './src/screen/HomeScreen';
+import { RoomScreen } from './src/screen/RoomScreen';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const Stack = createNativeStackNavigator();
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+function App(): React.JSX.Element {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Room" component={RoomScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
--- CineDateMobile/src/screen/RoomScreen.tsx
+++ CineDateMobile/src/screen/RoomScreen.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 import Video, { VideoRef } from 'react-native-video';
 import { useRoomStore } from '../store/UseRoomStore';
 import SocketService from '../services/socket';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 export const RoomScreen = ({ navigation }: any) => {
   const videoRef = useRef<VideoRef>(null);
@@ -11,8 +12,9 @@ export const RoomScreen = ({ navigation }: any) => {
     useRoomStore();
 
   // Socket'ten gelen zaman eşitlemesi durumunda videoyu ilgili saniyeye sar
+  // ✅ DOĞRU KULLANIM:
   useEffect(() => {
-    if (VideoRef.current && videoRef.current === currentTime) {
+    if (videoRef.current && currentTime !== undefined) {
       videoRef.current.seek(currentTime);
     }
   }, [currentTime]);
@@ -32,4 +34,63 @@ export const RoomScreen = ({ navigation }: any) => {
     setPlayerState(isPlaying, newTime);
     SocketService.emitPlayerAction('SEEK', newTime);
   };
+
+  return (
+    <View style={style.container}>
+      <Text style={style.headerTitle}> oda kodu: {roomId}</Text>
+
+      {streamUrl ? (
+        <Video
+          ref={videoRef}
+          source={{ uri: streamUrl, headers: headers || {} }}
+          paused={!isPlaying}
+          onSeek={seek => handleSeek(seek.currentTime)}
+          style={style.videoStyle}
+          controls={true}
+          resizeMode="contain"
+        ></Video>
+      ) : (
+        <View style={style.videoError}>
+          <Text style={style.errorText}>Video Bulunamadı</Text>
+        </View>
+      )}
+
+      {/* //manuel kontol pause/play */}
+      <View style={style.controls}>
+        <TouchableOpacity style={style.controlsButton}>
+          <Text style={style.buttonText}>
+            {isPlaying ? 'Durdur ⏸' : 'Oynat ▶'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#000', justifyContent: 'center' },
+  headerTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    paddingVertical: 20,
+    backgroundColor: '#1e1e1e',
+  },
+  videoStyle: { width: '100%', height: 260 },
+  videoError: {
+    height: 260,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#222',
+  },
+  errorText: { color: '#fff' },
+  controls: { flexDirection: 'row', justifyContent: 'center', padding: 20 },
+  controlsButton: {
+    backgroundColor: '#E50914',
+    paddingVertical: 24,
+    paddingHorizontal: 12,
+    borderRadius: 10,
+  },
+  buttonText: { color: '#fff' },
+});
